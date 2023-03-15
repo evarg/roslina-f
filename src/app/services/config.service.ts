fae11be0763aedc97788ea9f48e54346e32ee087
evarg <grave432@gmail.com>
2023-03-15 08:00:53 +0100

    config service

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -5,7 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class ConfigService {
 
-public API_URI : string = "http://api.roslina.com.pl/api/";
+//public API_URI : string = "http://api.roslina.com.pl/api/";
+public API_URI : string = "http://roslina-b.poligon/api/";
 
   constructor() { }
 }
